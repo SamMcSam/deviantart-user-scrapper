c7382cf39bab793eb2ff389ab3b261c24800913a
lesam <lesammakesgames@gmail.com>
2023-03-03 00:15:11 +0100

    simple save to mongodb

--- scrapper.js
+++ scrapper.js
@@ -1,5 +1,15 @@
 const deviantart = require('./model/deviantart');
+const mongoose = require('mongoose');
+const User = require('./model/user');
+require('dotenv').config();
+
+mongoose.connect( process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('mongo OK'))
+  .catch(() => console.log('Oops'));
 
 deviantart.friends("lesam").then((users) => {
-    console.log(users);
+    users.forEach(data => {
+        const savedUser = new User({name: data.user.username});
+        savedUser.save();
+    });
 });
